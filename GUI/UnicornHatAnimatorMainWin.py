89c2eb0ef69d9800ac7f8ffdd62b56dd44e383e3
indika2000 <indika.attale@gmail.com>
2018-02-18 20:26:06 +0000

    basic ip broadcast to see who is listening broadcast

--- GUI/UnicornHatAnimatorMainWin.py
+++ GUI/UnicornHatAnimatorMainWin.py
@@ -4,7 +4,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSlot
 import socket
-import pickle
+import time
 
 class App(QWidget):
 
@@ -22,6 +22,12 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
+        #find all connecting machines
+        try:
+            self.is_anybody_out_there()
+        except:
+            pass
+
         testbutton = QPushButton('Send Test Signal', self)
         testbutton.setToolTip('Test Signal Button')
         testbutton.move(100,70)
@@ -29,9 +35,25 @@ class App(QWidget):
 
         self.show()
 
+    #Signal to find all listening HATs
+    def is_anybody_out_there(self):
+        for e in range(39, 41):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                ip = "192.168.0.{}".format(e)
+                print('Attempting to connect to {}'.format(ip))
+                sock.settimeout(0.1)
+                try:
+                    testjson = '{ "action": "test", "data": [3, 3, 155, 233, 21] }'
+                    sock.connect((ip, 9999))
+                    sock.sendall(bytes(testjson + '\n', 'utf-8'))
+                except OSError as msg:
+                    print('Here!! {}'.format(msg))
+                    sock.close()
+
+
+
     @pyqtSlot()
     def on_testbutton_click(self):
-        testtup = pickle.dumps((3, 3, 155, 233, 21))
         testjson = '{ "action": "test", "data": [3, 3, 155, 233, 21] }'
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect(("192.168.0.40", 9999))
@@ -40,6 +62,8 @@ class App(QWidget):
                 sock.sendall(bytes(testjson + '\n', 'utf-8'))
             except:
                 pass
+
+
 
 
 if __name__ == '__main__':
